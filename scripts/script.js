bfc496bf960003de742179fb7704502d676ad66a
Victoria <wwaruri@gmail.com>
2021-07-26 18:55:14 +0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,77 @@
+$ (document).ready(function() {
+  $ ('#col1').click(function() {
+    $( '.description1').show();
+    $('#col1').hide();
+  
+  });
+});
+$ (document).ready(function() {
+  $('.description1').click(function() {
+    $('#col1').show();
+    $('.description1').hide();
+  });
+});
+$ (document).ready(function() {
+  $ ('#col2').click(function() {
+    $( '.description2').show();
+    $('#col2').hide();
+  
+  });
+});
+$ (document).ready(function() {
+  $('.description2').click(function() {
+    $('#col2').show();
+    $('.description2').hide();
+  });
+});
+$ (document).ready(function() {
+  $ ('#col3').click(function() {
+    $( '.description3').show();
+    $('#col3').hide();
+  
+  });
+});
+$ (document).ready(function() {
+  $('.description3').click(function() {
+    $('#col3').show();
+    $('.description3').hide();
+  });
+});
+$ (document).ready(function () {
+  $('.description1').css({
+    'text-align': 'center',
+  });
+});
+$ (document).ready(function () {
+  $ ('.header').css ({
+    'font-style': 'bold',
+    'text-align': 'center'
+  });
+});
+$ (document).ready(function () {
+  $('.description2').css({
+    'text-align': 'center',
+  });
+});
+$ (document).ready(function () {
+  $ ('.header2').css ({
+    'font-style': 'bold',
+    'text-align': 'center'
+  });
+});
+$ (document).ready(function () {
+  $('.description3').css({
+    'text-align': 'center',
+  });
+});
+$ (document).ready(function () {
+  $ ('.header3').css ({
+    'font-style': 'bold',
+    'text-align': 'center'
+  });
+});
+$ (document).ready(function() {
+  $ ('#portfolio')
+})
+
+
